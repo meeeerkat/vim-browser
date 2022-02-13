7d136bd8d754e9105de0832da8bb37423e693ab6
meeeerkat <meeeerkat@protonmail.com>
2022-02-13 19:55:27 +0100

    Using getenv to get home directory path instead of getpwuid

--- src/helpers/system.cpp
+++ src/helpers/system.cpp
@@ -1,13 +1,9 @@
-#include <unistd.h>
-#include <sys/types.h>
-#include <pwd.h>
-#include "helpers/system.hpp"
+#include <stdlib.h>
 
 
 namespace Helpers::System {
     const char *get_home_dir_path()
     {
-        struct passwd *pw = getpwuid(getuid());
-        return pw->pw_dir;
+        return getenv("HOME");
     }
 }
